84435b3a582984678a8789ebec718624e14ba0f0
Dmitry Chesnokov <dmitrijches@gmail.com>
2021-04-29 13:45:48 +0300

    modified Solution / add Условие

---
+++ JavaCollections/task32/task3211/Solution.java
@@ -0,0 +1,43 @@
+package com.javarush.task.task32.task3211;
+
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectOutputStream;
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.util.Arrays;
+
+/* 
+Целостность информации
+*/
+
+public class Solution {
+    public static void main(String... args) throws Exception {
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        ObjectOutputStream oos = new ObjectOutputStream(bos);
+        oos.writeObject(new String("test string"));
+        oos.flush();
+        System.out.println(compareMD5(bos, "5a47d12a2e3f9fecf2d9ba1fd98152eb")); //true
+
+    }
+
+    public static boolean compareMD5(ByteArrayOutputStream byteArrayOutputStream, String md5) throws Exception {
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        byte[] toByteArray = byteArrayOutputStream.toByteArray();
+        byte[] digest = md.digest(toByteArray);
+        StringBuilder sb = new StringBuilder();
+
+        for (byte b : digest) {
+            sb.append(String.format("%02x", b));
+        }
+
+        return sb.toString().equals(md5);
+
+        /*MessageDigest md = MessageDigest.getInstance("MD5");
+        byte[] toByteArray = byteArrayOutputStream.toByteArray();
+        byte[] digest = md.digest(toByteArray);
+        BigInteger bigInt = new BigInteger(1, digest);
+        String md5Hex = bigInt.toString(16);
+        return md5.equals(md5Hex);*/
+    }
+}
