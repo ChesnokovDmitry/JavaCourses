afedf236019d023ea21db450fc32e43aa8ff62b6
Dmitry Chesnokov <dmitrijches@gmail.com>
2021-06-27 21:31:40 +0300

    Recursion. Factorization

---
+++ JavaCollections/task34/task3403/Solution.java
@@ -0,0 +1,32 @@
+package com.javarush.task.task34.task3403;
+
+/* 
+Разложение на множители с помощью рекурсии
+*/
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        solution.recurse(132);
+    }
+
+    public void recurse(int n) {
+        int div = 2;
+
+        if (n > 1) {
+            if (n % div == 0) {
+                System.out.print(div + " ");
+                recurse(n / div);
+            } else {
+                while (n % div != 0)
+                    div++;
+
+                System.out.print(div + " ");
+                recurse(n / div);
+            }
+        }
+    }
+}
